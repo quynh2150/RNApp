abf1ecce4130c92fec469433fb239f7a603a7a51
Quynh Tran <quynhtran2150@gmail.com>
2019-05-15 20:41:27 +0700

    2nd commit

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, KeyboardAvoidingView } from 'react-native';
 
 export default class App extends React.Component {
+
+  state = {
+    message: '',
+    conversation: [{
+      key: 1,
+      user: 1,
+      name: "Quynh",
+      message: "Hello"
+    }, {
+      key: 2,
+      user: 2,
+      name: "Nguyen",
+      message: "Hi"
+    }]
+  }
+
+  sendMessage = () => {
+    const { message } = this.state;
+    this.setState({
+      conversation: [
+        ...this.state.conversation,
+        {
+          key: Math.random().toString(),
+          user: 1,
+          name: "Quynh",
+          message
+        }
+      ],
+      message: ''
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello world!</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+        <FlatList
+          data={this.state.conversation}
+          renderItem={({ item }) => <Text>{item.name}: {item.message}</Text>}
+        />
+        <TextInput 
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(message) => this.setState({ message })}
+          value={this.state.message} 
+          onSubmitEditing={this.sendMessage}
+        />
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -15,7 +56,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: '10%'
   },
 });
